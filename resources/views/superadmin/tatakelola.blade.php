783262080493eea66f8cd2e7b53b3008d4a42be9
fikriaulia09 <fikri.aulia1047@students.unila.ac.id>
2020-01-20 13:37:35 +0700

    halaman cobit 5

--- resources/views/superadmin/tatakelola.blade.php
+++ resources/views/superadmin/tatakelola.blade.php
@@ -101,8 +101,14 @@
                                         {{session('status')}}
                                     </div>
                                     </div>
+
+                                    <div class="panel-body">
+                                        <div class="alert alert-danger">
+                                            {{session('status')}}
+                                        </div>
+                                        </div>
                                     {{-- alert -> tindakan yang dilakukan --}}
-                                    
+
                                 @endif
                                 <div class="panel-body">
                                     <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
