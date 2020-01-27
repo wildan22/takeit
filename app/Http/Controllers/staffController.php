2f702322a858b5b059816b833c58acca5307ad4e
wildan22 <wildan.mutaqinn@gmail.com>
2020-01-27 16:28:29 +0700

    Staff Controller

--- app/Http/Controllers/staffController.php
+++ app/Http/Controllers/staffController.php
@@ -30,7 +30,44 @@ class staffController extends Controller{
     }
 
     public function showNewLaporan(){
-        return view('itstaff.new_laporan');
+        $periode_count = DB::table('periode_audit')->where('status','PROSES')->count();
+        if($periode_count != 0){
+            $periode = DB::table('periode_audit')->where('status','PROSES')->get();
+            $proses_ti = DB::table('proses_ti')->get();
+            return view('itstaff.new_laporan',['periode'=>$periode,'proses_ti'=>$proses_ti]);
+        }
+        else{
+            return redirect('/itstaff/laporan')->with('status','Tidak ada Audit yang sedang berjalan');
+        }
+    }
+
+    public function prosesTambahLaporan(Request $request){
+        $file = $request->file('laporan');
+        $tujuan_upload = 'file';
+        $ekstensi =$file->getClientOriginalExtension();
+        if($ekstensi == 'pdf'){
+            $newfilename = $request->judul_laporan.'.'.$ekstensi;
+            $upload = $file->move($tujuan_upload,$newfilename);
+            if($upload){
+                $tambahlaporan = DB::table('laporan')->insert([
+                    'nama_laporan'=>$request->judul_laporan,
+                    'id_proses_ti'=>$request->proses_ti,
+                    'id_periode_audit'=>$request->periode,
+                    'lokasi_laporan'=>$tujuan_upload.'/'.$newfilename
+                ]);
+                if($tambahlaporan == 1){
+                    return redirect('/itstaff/laporan')->with('status','Laporan Berhasil Ditambahkan');
+                }
+                else{
+                    return redirect('/itstaff/laporan')->with('status','Laporan Gagal Ditambahkan');
+                }
+            }
+            else{
+                return redirect('/itstaff/laporan')->with('status','Laporan Gagal');
+            }
+        }else{
+            echo 'NOT PDF';
+        }
     }
 
     public function showUbahPassowrd(){   
